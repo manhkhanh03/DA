a69368c0f7a4a2a6fb7378ab211db5fc4f1006ae
Manhkhanh <khanhmanh0705@gmail.com>
2022-10-29 07:13:03 +0700

    add

---
+++ js/home_page.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.autoplay').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
