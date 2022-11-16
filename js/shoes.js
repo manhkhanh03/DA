fb0d638468ec9dac5ddc12f46a84e6fd87261423
manhkhanh03 <khanhmanh0705@gmail.com>
2022-11-16 08:36:54 +0700

    add

--- js/shoes.js
+++ js/shoes.js
@@ -5,11 +5,13 @@ function hiddenFilter() {
         document.getElementById('hidden').style.display = 'unset';
         document.getElementById('result-filter').style.width = '60%';
         document.getElementById('hidden-filter').style.width = '20%';
+        document.getElementById('right-filter').style.width = '78%';
         document.getElementById('left-filter').style.display = 'block';
         document.getElementById('x').value = '1';
     } else {
         document.getElementById('result-filter').style.width = '72%';
-        document.getElementById('hidden-filter').style.width = '8%';
+        document.getElementById('hidden-filter').style.width = '2%';
+        document.getElementById('right-filter').style.width = '100%';
         document.getElementById('hidden').style.display = 'none';
         document.getElementById('left-filter').style.display = 'none';
         document.getElementById('x').value = '0';
@@ -29,20 +31,28 @@ function sortFilter() {
 }
 
 // Ẩn phong cách
-let btnHidden = document.querySelector('.fa-minus')
-btnHidden.addEventListener('click', function (event) {
-    event.currentTarget.classList.toggle('show');
-    document.getElementById('style').style.display = 'none';
-    document.getElementById('plus').style.display = 'block';
-    document.getElementById('minus').style.display = 'none';
+let btnHidden = document.getElementsByClassName('fa-minus')
+for (i = 0; i < btnHidden.length; i++) {
+    btnHidden[i].addEventListener('click', function (event) {    
+        let styles = document.getElementsByClassName('style')
+        let plus = document.getElementsByClassName('fa-plus')
+        for(j = 0; j < styles.length; j++) {
+            event.currentTarget.classList.toggle('show');
+            styles[j].style.display = 'none'
+            plus[j].style.display = 'block'
+        }
+    })
+}
 
-})
-
-let btnUnHidden = document.querySelector('.fa-plus')
-btnUnHidden.addEventListener('click', function (event) {
-    event.currentTarget.classList.toggle('show');
-    document.getElementById('style').style.display = 'flex';
-    document.getElementById('plus').style.display = 'none';
-    document.getElementById('minus').style.display = 'block';
-})
-
+let btnUnHidden = document.getElementsByClassName('fa-plus')
+for (i = 0; i < btnUnHidden.length; i++) {
+    btnUnHidden[i].addEventListener('click', function (event) {     
+        let styles = document.getElementsByClassName('style')
+        let minus = document.getElementsByClassName('fa-minus')
+        for(j = 0; j < styles.length; j++) {
+            event.currentTarget.classList.toggle('show');
+            styles[j].style.display = 'flex'
+            minus[j].style.display = 'block'
+        }
+    })
+}
